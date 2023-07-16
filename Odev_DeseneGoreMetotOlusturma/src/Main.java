e0877f021e17d3b923432f572235f20856d26f41
didem9 <132821536+didem9@users.noreply.github.com>
2023-07-16 17:52:42 +0300

    Add files via upload

---
+++ Odev_DeseneGoreMetotOlusturma/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+// Girilen sayı 0 veya negatif olduğu yere kadar girilen sayıdan 5 rakamını çıkarın.
+// Sayı negatif veya 0 olduktan sonra tekrar 5 ekleyin.
+
+public class Main {
+
+    static void recursiveMinus(int num, int temp) {
+
+        if (num > 0) {                                  // Her toplama işlemi sırasında son değeri yazdırıyoruz
+            System.out.print(num + " ");
+            recursiveMinus(num - 5, temp);
+        } else {
+            recursivePlus(num, temp);
+        }
+    }
+
+    static void recursivePlus(int num, int temp) {
+
+        if (temp >= num) {                              // Her çıkarma işlemi sırasında son değeri yazdırıyoruz
+            System.out.print(num + " ");
+            recursivePlus(num + 5, temp);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n, temp;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayı girin: ");
+        n = input.nextInt();
+        temp = n;
+
+        recursiveMinus(n, temp);
+
+    }
+}
+
+
